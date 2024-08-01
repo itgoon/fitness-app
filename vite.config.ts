2937ac4e73ba2aa1f498677fd5b67c8ee97ed323
gahyun <dfdf60708@gmail.com>
2024-08-01 17:26:54 +0900

    build: github actions build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
-import removeConsole from "vite-plugin-remove-console";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -32,6 +31,9 @@ export default ({ mode }) => {
           rewrite: (path) => path.replace(/^\/api/, "")
         }
       }
+    },
+    build: {
+      outDir: "build"
     }
   };
 
