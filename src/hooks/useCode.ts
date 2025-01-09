dc875130005994838c3c7a14d89dd78346121ec6
박건우 <white0581@naver.com>
2025-01-09 20:10:49 +0900

    feat: code API 연동

---
+++ src/hooks/useCode.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { CodeService } from 'src/service';
+import Store from 'src/store';
+
+export default function useCode() {
+  const [code, setCode] = useRecoilState(Store.Code.codeState);
+
+  useEffect(() => {
+    if (code) return;
+
+    loadCode();
+  }, []);
+
+  const loadCode = async () => {
+    const res = await CodeService.loadCode({});
+
+    setCode(res.data);
+  };
+
+  return code;
+}
